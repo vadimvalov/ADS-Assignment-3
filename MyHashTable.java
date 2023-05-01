493578af16132050dff54d001a17917351b85d2a
vadimvalov <115623536+vadimvalov@users.noreply.github.com>
2023-05-01 21:55:29 +0600

    Bugged int hash(K key)
    idk picked it from the lecture

--- MyHashTable.java
+++ MyHashTable.java
@@ -1,26 +1,59 @@
 public class MyHashTable<K, V> {
- private class HashNode<K, V> {
- private K key; 
- private V value;
- public HashNode(K key, V value) {
- this.key = key;
- this.value = value;
+    private class HashNode<K, V> {
+        private K key;
+        private V value;
+
+        public HashNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + key + " " + value + "}";
+        }
+
+        private HashNode<K, V>[] chainArray; // or Object[]
+        private int M = 11; // default number of chains
+        private int size;
+
+public MyHashTable() { 
+    this.chainArray = new HashNode<K,V>(key, value);
+    //
+ }
+
+public MyHashTable(int M) { 
+    this.chainArray = new.HashNode<K,V>(key,value);
+    //
+ }
+
+        private int hash(K key) {
+            int hash = 0;
+            String s = key.toString();
+            for (int i = 0; i < s.length(); i++) {
+                hash = s[i] + (31 * hash);
+            }
+            return Math.abs(hash) % M;
+        }
+
+        public void put(K key, V value) {
+
+        }
+
+        public V get(K key) {
+
+        }
+
+        public V remote(K key) {
+
+        }
+
+        public boolean contains(V value) {
+
+        }
+
+        public K getKey(V value) {
+
+        }
+    }
 }
-
-@Override 
-public String toString() {
-return "{" + key + " " + value + "}";
-}
-
-private HashNode<K, V> [] chainArray; // or Object[]
-private int M = 11; // default number of chains
-private int size;
-
-public MyHashTable() { ... }
-public MyHashTable(int M) { ... }
-private int hash(K key) { ... }
-public void put(K key, V value) { ... }
-public V get(K key) { ... }
-public V remote(K key) { ... }
-public boolean contains(V value) { ... }
-public K getKey(V value) { ... }
