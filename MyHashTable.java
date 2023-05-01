6f6e54d7ca6f0dcd8e33c840d9a6d53f07b52134
vadimvalov <115623536+vadimvalov@users.noreply.github.com>
2023-05-01 22:47:57 +0600

    bug fixing

--- MyHashTable.java
+++ MyHashTable.java
@@ -15,30 +15,31 @@ public class MyHashTable<K, V> {
         }
     }
 
-        private HashNode<K, V>[] chainArray; // or Object[]
-        private int M = 11; // default number of chains
-        private int size;
+    private HashNode<K, V>[] chainArray; // or Object[]
+    private int M = 11; // default number of chains
+    private int size;
 
-    public MyHashTable() { 
-    this.chainArray = new HashNode<K,V>(key, value);
-    //
+    public MyHashTable() {
+        this.chainArray = new HashNode[M];
+        this.size = 0;
     }
 
-public MyHashTable(int M) { 
-    this.chainArray = new.HashNode<K,V>(key,value);
-    this.M = M;
- }
+    public MyHashTable(int M) {
+        this.chainArray = new HashNode[M];
+        this.M = M;
+        this.size = 0;
+    }
 
-        private int hash(K key) {
-            int hash = 0;
-            String s = key.toString();
-            for (int i = 0; i < s.length(); i++) {
-                hash = s[i] + (31 * hash);
-            }
-            return Math.abs(hash) % M;
+    private int hash(K key) {
+        int hash = 0;
+        char[] s = key.toString().toCharArray();
+        for (int i = 0; i < s.length; i++) {
+            hash = s[i] + (31 * hash);
         }
+        return Math.abs(hash) % M;
+    }
 
-       public void put(K key, V value) {
+    public void put(K key, V value) {
         int index = hash(key);
         HashNode<K, V> current = chainArray[index];
         while (current != null) {
@@ -54,7 +55,7 @@ public MyHashTable(int M) {
         size++;
     }
 
-        public V get(K key) {
+    public V get(K key) {
         HashNode<K, V> current = chainArray[hash(key)];
         while (current != null) {
             if (current.key.equals(key)) {
@@ -84,7 +85,7 @@ public MyHashTable(int M) {
         return null;
     }
 
-        public boolean contains(V value) {
+    public boolean contains(V value) {
         for (int i = 0; i < chainArray.length; i++) {
             HashNode<K, V> current = chainArray[i];
             while (current != null) {
@@ -96,7 +97,6 @@ public MyHashTable(int M) {
         }
         return false;
     }
-
 
     public K getKey(V value) {
         for (int i = 0; i < chainArray.length; i++) {
@@ -110,5 +110,5 @@ public MyHashTable(int M) {
         }
         return null;
     }
-    
+
 }
