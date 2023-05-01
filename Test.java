7e24ab08a6e9693fce80ca38c10c2103569cce41
vadimvalov <115623536+vadimvalov@users.noreply.github.com>
2023-05-01 23:16:35 +0600

    TESTING #1
    фейл какойт. говорит не существует, хотя жб должно быть.

--- Test.java
+++ Test.java
@@ -1,3 +1,28 @@
+import java.util.Random;
+
 public class Test {
-    
+    private int value;
+    public Test(int value){
+        this.value = value;
+    }
+    public static void main(String[] args) {
+        MyHashTable<Test, Student> table = new MyHashTable<>();
+        Random random = new Random();
+        for (int i = 0; i < 10001; i++) {
+            int key = random.nextInt(10001);
+            Test test = new Test(key);
+            Student value = new Student("V. V.");
+            table.put(test, value);
+        }
+
+        int rand = random.nextInt(10001);
+        Test testKey = new Test(rand);
+        Student studenttest = table.get(testKey);
+        if( studenttest != null){
+            System.out.println("Student : " + studenttest.getName());
+        } else {
+            System.out.println("No student with this ID");
+        }
+
+    }
 }
